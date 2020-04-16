a142791bea268c12e2c4978ffee1dd73fe3556e9
yura_borovoy <ysborovoy@gmail.com>
2020-04-16 12:12:43 +0300

    We can convert a number of any base to a number of any base

--- src/com/numeral_system_converter/Converter.java
+++ src/com/numeral_system_converter/Converter.java
@@ -3,56 +3,84 @@ package com.numeral_system_converter;
 import java.util.Scanner;
 
 public class Converter {
-    private Scanner scan = new Scanner(System.in);
+
+    private int sourceRadix;
+    private String sourceNumber;
+    private int targetRadix;
+    private final Scanner scan = new Scanner(System.in);
 
     public void goConvert() {
-        int sourceRadix = Integer.parseInt(scan.nextLine());
-        int sourceNumber = Integer.parseInt(scan.nextLine());
-        int targetRadix = Integer.parseInt(scan.nextLine());
+        sourceRadix = Integer.parseInt(scan.nextLine());
+        sourceNumber = scan.nextLine();
+        targetRadix = Integer.parseInt(scan.nextLine());
+        double decimalNumber = 0.0;
         //translate the sourceRadix to decimalRadix (work with sourceNumber)
-        int decimalNumber = translateToDecimalRadix(sourceRadix, sourceNumber);
-        //translate the decimalRadix to targetRadix
-        String result = translateToTargetRadix(decimalNumber, targetRadix);
-        System.out.println(result);
-        /*
-        switch(radix) {
-            case 16:
-                System.out.println("0x" + result);
-                break;
-            case 8:
-                System.out.println("0" + result);
-                break;
-            case 2:
-                System.out.println("0b" + result);
-                break;
-            default:
-                break;
-        }
-
-         */
-    }
-
-    public int translateToDecimalRadix (int sourceRadix, int sourceNumber) {
-        if (sourceRadix != 1) {
-            return Integer.parseInt(String.valueOf(sourceNumber), sourceRadix);
-        } else {
-            String[] massiveSource = String.valueOf(sourceNumber).split("");
-            int result = 0;
-            for (String s : massiveSource) {
-                result += Integer.parseInt(s);
+        if (sourceRadix == 10) {
+            decimalNumber = Double.parseDouble(sourceNumber);
+        } else if (sourceRadix == 1) {
+            for (int i = 0; i < sourceNumber.length(); i++) {
+                decimalNumber += 1;
             }
-            return result;
-        }
-    }
-    public String translateToTargetRadix (int decimalNumber, int targetRadix) {
-        if (targetRadix != 1) {
-            return Integer.toString(decimalNumber, targetRadix);
         } else {
-
-            return "1".repeat(Math.max(0, decimalNumber));
+            decimalNumber = translateToDecimalRadix();
         }
+        //translate the decimalRadix to targetRadix
+        System.out.println(translateToTargetRadix(decimalNumber));
     }
 
+    public double translateToDecimalRadix() {
+        if (sourceNumber.matches("[0-9a-z]*\\.[0-9a-z]*")) {
+            String[] wholeAndFraction = sourceNumber.toLowerCase().split("\\.");
+            int wholePart = Integer.parseInt(wholeAndFraction[0], sourceRadix);//translate wholePart to decimalWholePart
+            StringBuilder fractionPart = new StringBuilder();
+            fractionPart.append((wholeAndFraction[1]).toLowerCase());
+            double decimalFractionPart = 0.0;
+            int countPow = 1;
+            //divide each unit of the fractionPart by the sourceRadix^countPow
+            for (int i = 0; i < fractionPart.length(); i++) {
+                if (fractionPart.charAt(i) >= 'a' && fractionPart.charAt(i) <= 'z') {
+                    decimalFractionPart += Integer.parseInt(String.valueOf(fractionPart.charAt(i) - 87)) / Math.pow(sourceRadix, countPow);
+                } else {
+                    decimalFractionPart += Integer.parseInt(String.valueOf(fractionPart.charAt(i))) / Math.pow(sourceRadix, countPow);
+                }
+                countPow++;
+            }
+            return wholePart + decimalFractionPart;
+        } else {
+            return Integer.parseInt(sourceNumber, sourceRadix);
+        }
+    }
+        public String translateToTargetRadix(double decimalNumber){
+            int wholePart = (int) decimalNumber;
+            if (sourceNumber.matches("[0-9a-z]*\\.[0-9a-z]*")) {
+                double fractionPart = decimalNumber - wholePart;
+                StringBuilder fractionTargetPart = new StringBuilder();
+                //Multiply the fractional part by the new base.
+                //The integer part of the result is the first digit
+                // (or letter if the integer part is greater than 9)
+                // in the fractional part of a number in the new base.
+                for (int i = 0; i < 5; i++) {
+                    fractionPart = fractionPart * targetRadix;
+                    if ((int) fractionPart > 9) {
+                        fractionTargetPart.append((char) (87 + (int) fractionPart));
+                    } else {
+                        fractionTargetPart.append((int) fractionPart);
+                    }
+                    fractionPart -= (int) fractionPart;
+                }
+                if (targetRadix != 1) {
+                    return Integer.toString(wholePart, targetRadix) + "." + fractionTargetPart.toString();
+                } else {
+                    return "1".repeat(Math.max(0, wholePart));
+                }
+            } else {
+                if (targetRadix != 1) {
+                    return Integer.toString(wholePart, targetRadix);
+                } else {
 
-}
+                    return "1".repeat(Math.max(0, wholePart));
+                }
+            }
+        }
+    }
 
